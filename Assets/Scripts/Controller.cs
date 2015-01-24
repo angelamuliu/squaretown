dc0cb683ea4cd9740cb3246a911a6cf8bd271b7b
fgomezfr <fgomezfr@outlook.com>
2015-01-24 17:09:38 -0500

    controller updates and power ups

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,12 +9,13 @@ public class Controller : MonoBehaviour {
 	public KeyCode down;
 	public KeyCode right;
 	public KeyCode left;
-	public float speed = 0;
+
 	public float jumpForce = 1;
 	public float movespeed = 1;
 
 	// Keeping track of player contact with ground
 	bool grounded = false; // Start false b/c player starts out falling
+	private Vector2 delta;
 	public Transform side1Check;
 	public Transform side2Check;
 	public Transform side3Check;
@@ -23,45 +24,67 @@ public class Controller : MonoBehaviour {
 
 	void Start() {
 		colorshape = GetComponent <ColorShape> ();
+		delta = Vector2.zero;
 	}
 
 	void FixedUpdate () {
-		// Checking if any of its sides are touching the ground, first detect which is the lowest to use
-		if (side1Check.position.y < side2Check.position.y) {
-			if (side1Check.position.y < side3Check.position.y) {
-				grounded = Physics2D.OverlapCircle (side1Check.position, groundradius, whatIsGround);
-			} else { // side1 is greater than side 3 but less than side 2
-				grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
-			}
-		} else { // side 1 is greather than side 2
-			if (side2Check.position.y < side3Check.position.y) {
-				grounded = Physics2D.OverlapCircle (side2Check.position, groundradius, whatIsGround);
-			} else { // side 2 is less than side 1 but greater than side 3
-				grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
-			}
+
+		rigidbody2D.velocity = new Vector2( delta.x, rigidbody2D.velocity.y + delta.y );
+
+		// wall jumping if you are a square!
+		if (colorshape.color == 1)
+		{
+			grounded = Physics2D.OverlapCircle (side1Check.position, groundradius, whatIsGround)
+					|| Physics2D.OverlapCircle (side2Check.position, groundradius, whatIsGround)
+					|| Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
 		}
+		else
+		{
+			// Checking if any of its sides are touching the ground, first detect which is the lowest to use
+			if (side1Check.position.y < side2Check.position.y) {
+				if (side1Check.position.y < side3Check.position.y) {
+					grounded = Physics2D.OverlapCircle (side1Check.position, groundradius, whatIsGround);
+				} else { // side1 is greater than side 3 but less than side 2
+					grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
+				}
+			} else { // side 1 is greather than side 2
+				if (side2Check.position.y < side3Check.position.y) {
+					grounded = Physics2D.OverlapCircle (side2Check.position, groundradius, whatIsGround);
+				} else { // side 2 is less than side 1 but greater than side 3
+					grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
+				}
+			}
+		}		
 	}
 
 	void Update() {
+
+		float x = 0.0f, y = 0.0f;
+
 		// Horizontal movement
 		if (Input.GetKey (right)) 
 		{
-			rigidbody2D.velocity = new Vector2 (movespeed, rigidbody2D.velocity.y);
+			x = movespeed;
 		} 
 		else if (Input.GetKey (left)) {
-			rigidbody2D.velocity = new Vector2 (-movespeed, rigidbody2D.velocity.y);
+			x = -movespeed;
 		}
 		else {
-			rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
+			x = 0;
 		}
+		if (colorshape.color == 2)
+			x *= 2;
 
 		// Vertical movement
 		if (grounded && Input.GetKey (up)) {
-			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, jumpForce);
+			y = jumpForce;
 		} else {
-			// Maybe put triples jumps here one day...
+			y = 0;
 		}
+		if (colorshape.color == 0)
+			y *= 2;
 
+		delta = new Vector2(x, y);
 	}
 
 	// Deactivate objects player collides with if they are pickups
