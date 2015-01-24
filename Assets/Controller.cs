c20a7bf7c656ec352428f27466667acc7eb63f26
Angela Liu <angelamuliu@gmail.com>
2015-01-24 11:10:44 -0500

    Basic movement and physic enabled benbluebox

---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode right;
+	public KeyCode left;
+	public float speed = 0;
+	
+	void FixedUpdate () {
+		if (Input.GetKey(up)) {
+			transform.Translate(new Vector2(0.0f, speed+0.1f));
+		}
+		
+		if (Input.GetKey(down)) {
+			transform.Translate(new Vector2(0.0f, (-1)*(speed+0.1f)));
+		}
+		
+		if (Input.GetKey(right)) {
+			transform.Translate(new Vector2(speed+0.1f, 0.0f));
+		}
+		if (Input.GetKey(left)){
+			transform.Translate(new Vector2((-1)*(speed+0.1f), 0.0f));
+		}
+	}
+}
