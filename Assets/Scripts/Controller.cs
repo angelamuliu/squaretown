77bf6ab328423715ec7d245b62f79ada10258c26
Angela Liu <angelamuliu@gmail.com>
2015-01-24 13:47:07 -0500

    Corrected groundcheck for all three sides of player

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -14,13 +14,28 @@ public class Controller : MonoBehaviour {
 
 	// Keeping track of player contact with ground
 	bool grounded = false; // Start false b/c player starts out falling
-	public Transform groundCheck;
+	public Transform side1Check;
+	public Transform side2Check;
+	public Transform side3Check;
 	float groundradius = 0.2f;
 	public LayerMask whatIsGround;
 
 
 	void FixedUpdate () {
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundradius, whatIsGround);
+		// Checking if any of its sides are touching the ground, first detect which is the lowest to use
+		if (side1Check.position.y < side2Check.position.y) {
+			if (side1Check.position.y < side3Check.position.y) {
+				grounded = Physics2D.OverlapCircle (side1Check.position, groundradius, whatIsGround);
+			} else { // side1 is greater than side 3 but less than side 2
+				grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
+			}
+		} else { // side 1 is greather than side 2
+			if (side2Check.position.y < side3Check.position.y) {
+				grounded = Physics2D.OverlapCircle (side2Check.position, groundradius, whatIsGround);
+			} else { // side 2 is less than side 1 but greater than side 3
+				grounded = Physics2D.OverlapCircle (side3Check.position, groundradius, whatIsGround);
+			}
+		}
 	}
 
 	void Update() {
