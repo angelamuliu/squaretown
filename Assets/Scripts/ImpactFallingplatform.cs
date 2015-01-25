f5b697c4bd5e3b5ee5fdd412a2062d9dd21a878c
mkim1994 <mkim2@andrew.cmu.edu>
2015-01-25 11:35:47 -0500

    MORE TESTING with pentagons

--- Assets/Scripts/ImpactFallingplatform.cs
+++ Assets/Scripts/ImpactFallingplatform.cs
@@ -19,6 +19,8 @@ public class ImpactFallingplatform : MonoBehaviour {
 
 	SpriteRenderer mySprite;
 	PolygonCollider2D myCollider;
+
+	TriggerStalactite trig; 
 	
 	void Start () {
 		mySprite = GetComponent<SpriteRenderer>();
@@ -26,43 +28,57 @@ public class ImpactFallingplatform : MonoBehaviour {
 		startx = transform.position.x;
 		starty = transform.position.y;
 		startRotation = transform.rotation;
+
+		trig = GetComponent<TriggerStalactite>();
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if(trig.stalacFall == true){
+			//rigidbody2D.WakeUp ();
+			rigidbody2D.Sleep();
+		}
+
 		// Fading out while falling
 		if (falling) {
 			// Dark tint fading to transparency
 			mySprite.color = new Color(242.0f/255, 242.0f/255, 242.0f/255, fadespeed/30f);
-			fadespeed--;
 		}
 	}
 
+	void Update(){
+
+
+	}
+
+
 	IEnumerator OnCollisionEnter2D(Collision2D other) {
+
 		if (other.gameObject.tag == "Stalactite") {
 			// Player can safely sit on this platform for a bit
 			mySprite.color = darktint;
 			yield return new WaitForSeconds (safetime);
 			falling = true;
-			rigidbody2D.WakeUp();
+			//rigidbody2D.WakeUp();
 
 			yield return new WaitForSeconds (jumpdelay);
 			myCollider.enabled = false;
 
 			yield return new WaitForSeconds (respawntime); // Set the fall time
-			respawn ();
+			//respawn ();
 		}
 	}
 
 	// Reset the platform
-	void respawn() {
+/*	void respawn() {
 		falling = false; fadespeed = 30;
 		rigidbody2D.Sleep();
 		mySprite.color = new Color (1, 1, 1, 1);
 		transform.position = new Vector3(startx, starty, 0);
 		transform.rotation = startRotation;
 		myCollider.enabled = true;
-	}
+	}*/
 
 
 	
---
+++ Assets/TriggerStalactite.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerStalactite : MonoBehaviour {
+
+	public bool stalacFall = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player"){
+			stalacFall = true;
+			
+		}
+		else{
+			stalacFall = false;
+		}
+	}
+}
