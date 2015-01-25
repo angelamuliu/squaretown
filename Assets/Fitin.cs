f98c7aaa84cff38a6cd2f271bcef05f514ca2312
Angela Liu <angelamuliu@gmail.com>
2015-01-24 22:59:44 -0500

    Starting work on fitin checker

---
+++ Assets/Fitin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fitin : MonoBehaviour {
+
+	public Transform playerChecker;
+	float checkradius = 0.2f;
+	public LayerMask whatIsPlayer;
+
+	private bool fitin = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (fitin) {
+			Debug.Log ("GAME OVER");
+		}
+	
+	}
+
+	private void CollisionDetection() {
+		fitin = Physics2D.OverlapCircle (playerChecker.position, checkradius, whatIsPlayer);
+	}
+}
