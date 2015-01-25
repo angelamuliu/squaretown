d35807cc3afc9d243fd4a5adf3c0b46989441230
Angela Liu <angelamuliu@gmail.com>
2015-01-24 20:18:10 -0500

    Added falling platforms

---
+++ Assets/Scripts/Fallingplatform.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fallingplatform : MonoBehaviour {
+
+
+	public float safetime = 1; // Time player has until platform starts falling
+	public float jumpdelay = 1; // Time palyer has while platform is falling, can still jump
+	public float respawntime = 5; // Time it'll take for platform to respawn
+
+	private bool falling = false;
+	private int fadespeed = 30;
+
+	private float startx;
+	private float starty;
+
+	private Color darktint = new Color(242.0f/255, 242.0f/255, 242.0f/255, 1);
+
+	SpriteRenderer mySprite;
+	BoxCollider2D boxcollider;
+	
+	void Start () {
+		mySprite = GetComponent<SpriteRenderer> ();
+		boxcollider = GetComponent<BoxCollider2D>();
+		startx = transform.position.x;
+		starty = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Fading out while falling
+		if (falling) {
+			// Dark tint fading to transparency
+			mySprite.color = new Color(242.0f/255, 242.0f/255, 242.0f/255, fadespeed/30f);
+			fadespeed --;
+		}
+	}
+
+	IEnumerator OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			// Player can safely sit on this platform for a bit
+			mySprite.color = darktint;
+			yield return new WaitForSeconds (safetime);
+			falling = true;
+			rigidbody2D.isKinematic = false;
+
+			yield return new WaitForSeconds (jumpdelay);
+			boxcollider = GetComponent<BoxCollider2D>();
+			boxcollider.enabled = false;
+
+			yield return new WaitForSeconds (respawntime); // Set the fall time
+			respawn ();
+		}
+	}
+
+	// Reset the platform
+	void respawn() {
+		falling = false; fadespeed = 30;
+		rigidbody2D.isKinematic = true;
+		mySprite.color = new Color (1, 1, 1, 1);
+		transform.position = new Vector3(startx, starty, 0);
+		boxcollider.enabled = true;
+	}
+
+
+	
+}
