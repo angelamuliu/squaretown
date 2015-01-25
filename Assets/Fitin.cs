d2c6189f9baef48fc2a70bf80d42b704a5fe9fea
Angela Liu <angelamuliu@gmail.com>
2015-01-25 00:59:31 -0500

    Homes are now triggering

--- Assets/Fitin.cs
+++ Assets/Fitin.cs
@@ -3,28 +3,14 @@ using System.Collections;
 
 public class Fitin : MonoBehaviour {
 
-	public Transform playerChecker;
-	float checkradius = 0.2f;
-	public LayerMask whatIsPlayer;
 
 	private bool fitin = false;
+//	public MainMenu gameEngine;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		CollisionDetection ();
-		if (fitin) {
-			Debug.Log ("GAME OVER");
-		} else {
-			Debug.Log ("no..");
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			Debug.Log ("GAME OVER1");
 		}
 	}
 
-	void CollisionDetection() {
-		fitin = Physics2D.OverlapCircle (playerChecker.position, checkradius, whatIsPlayer);
-	}
 }
